d42e71b9f4f14b816352aa836fb6bf044b3147c7
Stryker Stinnette <strykerkent@gmail.com>
2021-08-17 16:30:16 -0500

    added in cart array, purchase event, and remove event

--- js/script.js
+++ js/script.js
@@ -37,6 +37,26 @@ const items = [
   },
 ];
 
+// const items_in_cart = [];
+const items_in_cart = [
+  {
+    line_number: 1,
+    item_id: 4,
+    name: 'Skateboard',
+    price: 200,
+    quantity: 1,
+  },
+  {
+    line_number: 2,
+    item_id: 1,
+    name: 'Shirt',
+    price: 26,
+    quantity: 1,
+  },
+];
+
+// let count = 0;
+// let price = 0;
 let count = 2;
 let price = 226;
 
@@ -73,6 +93,14 @@ for (let i = 0; i < allbtn_shop.length; i++) {
 
     document.querySelector('#cart_main').appendChild(div);
 
+    //add item to items_in_cart
+    items_in_cart.push({
+      line_number: count,
+      item_id: item[0].id,
+      name: `${item[0].name}`,
+      price: item[0].price,
+      quantity: 1,
+    });
     //update total
     document.querySelector('#total').innerHTML = `$${price}`;
   });
@@ -94,3 +122,43 @@ document.getElementById('close_cart').addEventListener('click', () => {
   //add class hide
   document.getElementById('cart').classList.add('hide');
 });
+
+//purchase click
+document.getElementById('checkout').addEventListener('click', () => {
+  alert(`Fake purchase for $${price} made. Thank you.`);
+  location.reload();
+});
+
+//remove button logic
+document.addEventListener('click', (e) => {
+  if (e.target && e.target.id.includes('remove')) {
+    //remove item from array
+    // get line number
+    let line_number = e.target
+      .closest('.cart_row')
+      .getAttribute('data-line_number');
+    // find index of line number
+    let index = items_in_cart.findIndex(
+      (items_in_cart) => items_in_cart.line_number == line_number
+    );
+    items_in_cart.splice(index, 1);
+    // console.log(items_in_cart);
+
+    // remove item from cart visually
+    e.target.closest('.cart_row').remove();
+
+    // recalculate total
+    let total = 0;
+    if (items_in_cart.length > 0) {
+      total = items_in_cart
+        .map((x) => x.price)
+        .reduce((accumulator, currentTotal) => accumulator + currentTotal);
+    }
+    console.log(total);
+    price = total;
+    document.querySelector('#total').innerHTML = `$${total}`;
+  }
+});
+
+//todo: quantity change click
+//todo: update cart count in header
